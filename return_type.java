b97441a77c45d909eaf59ec81babc265015909b2
Vijay Prasad <75656901+vijayprasadvitstudent@users.noreply.github.com>
2021-07-14 22:04:36 +0530

    Add files via upload

---
+++ return_type.java
@@ -0,0 +1,26 @@
+class student
+{
+    int m1,m2,m3, total;
+    void process(int mark1, int mark2, int mark3)
+    {
+        m1=mark1;
+        m2=mark2;
+        m3=mark3;
+    }
+    int display()
+    {
+        total=m1+m2+m3;
+        System.out.println("The total marks are " + total);
+        return total;
+    }
+}
+
+public class return_type {
+    public static void main(String []args)
+    {
+        int t;
+        student s=new student();
+       s.process(90,97,99);
+        t=s.display();
+    }
+}
